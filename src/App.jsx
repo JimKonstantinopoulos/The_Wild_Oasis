449a2b6ffbaf1ca061514f118b43fb1a56649c89
dimKonstantinopoulos <dimkonstantinopoulos@gmail.com>
2024-12-17 21:47:37 +0200

    Implement pre-defined files to structure the web application/conigure "styled components"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 5rem;
+  font-weight: 600;
+`;
+
 function App(){
-  return <p>yo</p>;
+  return <div><H1>YYOOOO</H1></div>;
 }
 
 export default App;
