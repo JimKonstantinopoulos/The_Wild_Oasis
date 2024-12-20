6b74c9132a06cace67be78e85530800f4bacdb93
dimKonstantinopoulos <dimkonstantinopoulos@gmail.com>
2024-12-20 14:49:23 +0200

    Implement and configure "Styled Components" library

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ const H1 = styled.h1`
   font-weight: 600;
 `;
 
-function App(){
-  return <div><H1>YYOOOO</H1></div>;
+function App() {
+  return (
+    <div>
+      <H1>Hello world</H1>
+    </div>
+  );
 }
 
 export default App;
