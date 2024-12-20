ef05d7c11065cdee1a91e249de21f61ac4d8e8f7
dimKonstantinopoulos <dimkonstantinopoulos@gmail.com>
2024-12-20 18:17:43 +0200

    Build main part of the header using "Styled Components"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
-const H1 = styled.h1`
-  font-size: 5rem;
-  font-weight: 600;
+const StyledApp = styled.main`
+  padding: 20px;
 `;
 
 function App() {
   return (
-    <div>
-      <H1>Hello world</H1>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("check in")}>Check in</Button>
+              <Button onClick={() => alert("check out")}>Check out</Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <form>
+              <Heading as="h3">Form</Heading>
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
   );
 }
 
