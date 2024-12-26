de1217ba6f9d3b17e068b38bb8b746ea9383e4bb
dimKonstantinopoulos <dimkonstantinopoulos@gmail.com>
2024-12-26 19:43:45 +0200

    Implement the useOutsideClick custom hook for modal extra closing functionality

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,7 +1,15 @@
-import { cloneElement, createContext, useContext, useState } from "react";
+import {
+  cloneElement,
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -76,12 +84,13 @@ function Open({ children, opens: openWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close, true);
 
   if (name !== openName) return null;
-
+  //Use portal to make Modal more reusable by displaying on body level of DOM tree (while still existing on the original position at Component tree)
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
