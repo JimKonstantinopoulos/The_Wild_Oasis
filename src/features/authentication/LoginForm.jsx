7d84d4e531c725d4b2f6324d4448214d9825904f
dimKonstantinopoulos <dimkonstantinopoulos@gmail.com>
2025-01-02 20:45:57 +0200

    Display login page

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -32,7 +32,9 @@ function LoginForm() {
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button variation="primary" size="large">
+          Login
+        </Button>
       </FormRowVertical>
     </Form>
   );
