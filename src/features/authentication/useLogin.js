09beed0d0ec4960f151489a33b65609c5d7b10d0
dimKonstantinopoulos <dimkonstantinopoulos@gmail.com>
2025-01-02 23:15:13 +0200

    Build Signup for to create new user

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,14 +1,19 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { login as loginApi } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { isLoading, mutate: login } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: () => navigate("/dashboard", { replace: true }),
+    onSuccess: (user) => {
+      //Manually change the cache of react query to avoid user appearing null at the start of the login
+      queryClient.setQueryData(["user"], user.user);
+      navigate("/dashboard", { replace: true });
+    },
     onError: () => toast.error("Provided email or password are incorrect"),
   });
 
