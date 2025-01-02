5eecf9887f72a66b979a45c054559fdf7ddbf274
dimKonstantinopoulos <dimkonstantinopoulos@gmail.com>
2025-01-02 21:22:22 +0200

    Build LoginForm to login user to supabase via authentication

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -3,12 +3,20 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
+import useLogin from "./useLogin";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("jim@test.com");
+  const [password, setPassword] = useState("jimproaek");
+  const { isLoading, login } = useLogin();
 
-  function handleSubmit() {}
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+
+    login({ email, password });
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -19,6 +27,7 @@ function LoginForm() {
           // This makes this form better for password managers
           autoComplete="username"
           value={email}
+          disabled={isLoading}
           onChange={(e) => setEmail(e.target.value)}
         />
       </FormRowVertical>
@@ -28,12 +37,13 @@ function LoginForm() {
           id="password"
           autoComplete="current-password"
           value={password}
+          disabled={isLoading}
           onChange={(e) => setPassword(e.target.value)}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button variation="primary" size="large">
-          Login
+        <Button variation="primary" size="large" disabled={isLoading}>
+          {!isLoading ? "Login" : <SpinnerMini />}
         </Button>
       </FormRowVertical>
     </Form>
---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,18 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { login as loginApi } from "../../services/apiAuth";
+import toast from "react-hot-toast";
+
+export function useLogin() {
+  const navigate = useNavigate();
+
+  const { isLoading, mutate: login } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: () => navigate("/dashboard"),
+    onError: () => toast.error("Provided email or password are incorrect"),
+  });
+
+  return { isLoading, login };
+}
+
+export default useLogin;
