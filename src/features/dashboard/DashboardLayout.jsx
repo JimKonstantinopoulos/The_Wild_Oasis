88f9af8dc919985d21a3f7c28e823f4a1ecad7a8
dimKonstantinopoulos <dimkonstantinopoulos@gmail.com>
2025-01-03 21:28:56 +0200

    Implement basic structure of Dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashBoardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today`s activity</div>
+      <div>Chart stay duration</div>
+      <div>Chart of sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashBoardLayout;
